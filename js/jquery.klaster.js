1661f957a2d2d25ef7eaa811eb6a35002139e950
akindziora <alexander.kindziora@crowdguru.com>
2012-12-12 15:17:58 +0100

    timeout by last change

--- js/jquery.klaster.js
+++ js/jquery.klaster.js
@@ -5,9 +5,9 @@
  */
 (function( $ ){
     $.fn.klaster = function( child ) {
-        var self = this;
+        var self = this, cls = {};
         
-        self.info = {
+        cls.info = {
             'name' : 'klaster.js',
             'version' : '0.3',
             'tag' : 'alpha',
@@ -16,16 +16,16 @@
             'copyright' : 'author'
         };
         
-        self.values = {};
+        cls.values = {};
     
-        self.get = function(name, value) {
-            return ((typeof self[name] !== 'undefined') ? self[name] : value);
+        cls.get = function(name, value) {
+            return ((typeof cls[name] !== 'undefined') ? cls[name] : value);
         };
     
         /*
      * gets executed before an event is triggered
      */
-        self.pre_trigger = function(e) {
+        cls.pre_trigger = function(e) {
         
             if(typeof child.pre_trigger !== "undefined") 
                 return child.pre_trigger.call(this, e);
@@ -36,7 +36,7 @@
      * gets executed after a change on dom objects
      * "this" is the dom element responsible for the change
      */
-        self.changed = function() {
+        cls.changed = function() {
         
             if(typeof child.filter_changed !== "undefined") 
                 return child.filter_changed.call(this);
@@ -49,30 +49,34 @@
      *@description one common callback for changed is an ajax call with all values to a REST backend to update data
      * 
      */
-        self.recognizeChange = {
-            changed: function(el) {
-                self.changed.call(el);
-                delete this.timeoutID;
-            },
-            setup: function(el) {
-                this.cancel();
-                var me = this;
-                this.timeoutID = window.setTimeout(function(msg) {
-                    me.changed(msg);
-                }, self.get('delay', 1000), el);
-            },
-            cancel: function() {
-                if(typeof this.timeoutID == "number") {
-                    window.clearTimeout(this.timeoutID);
-                    delete this.timeoutID;
+        cls.recognizeChange = function(){
+            var mio = this;
+            
+            mio.changed = function(el) {
+                cls.changed.call(el);
+                delete cls.timeoutID;
+            };
+            
+            mio.cancel = function() {
+                if(typeof cls.timeoutID == "number") {
+                    window.clearTimeout(cls.timeoutID);
+                    delete cls.timeoutID;
                 }
+            };
+            
+            mio.setup = function(el) {
+                mio.cancel();
+                cls.timeoutID = window.setTimeout(function(msg) {
+                    mio.changed(msg);
+                }, cls.get('delay', 1000), el);
             }
-        };
+            return mio;
+        }();
     
         /*
      * gets executed after an event is triggered
      */
-        self.post_trigger = function(e, result) {
+        cls.post_trigger = function(e, result) {
             var seen = [];
             $(this).attr('data-value', JSON.stringify(result, function(key, val) {
                 if (typeof val == "object") {
@@ -83,9 +87,9 @@
                 return val
             }));
         
-            if(result != self.values[$(this).attr('data-name')]){
-                self.recognizeChange.setup($(this));
-                self.values[$(this).attr('data-name')] = result;
+            if(result != cls.values[$(this).attr('data-name')]){
+                cls.recognizeChange.setup($(this));
+                cls.values[$(this).attr('data-name')] = result;
             }
         
             if(typeof child.post_trigger !== "undefined") 
@@ -97,7 +101,7 @@
         /**
          *dispatch events for dom element
          */
-        self.dispatchEvents = function() {
+        cls.dispatchEvents = function() {
             var events = $(this).attr('data-on').split(','), i = 0, event = "", FinalEvents = {};
             var method = "", parts = "";
         
@@ -119,7 +123,7 @@
         /**
      * find all filters and init there configs
      */
-        self.dispatchFilter = function(byElement) {
+        cls.dispatchFilter = function(byElement) {
         
             if(typeof byElement === 'undefined')
                 byElement = $('[data-filter]');
@@ -140,12 +144,12 @@
         /**
      * bind dom to matching methods
      */
-        self.bind = function(element) {
+        cls.bind = function(element) {
             var events = {}, event = {}, name = "", method ="", fi = "", filterObj = {};
         
-            child = child(self);
+            child = child(cls);
         
-            self.filter = self.dispatchFilter(element);
+            cls.filter = cls.dispatchFilter(element);
         
             /* variable injection via lambda function factory used in iteration */
             var factory = function (me, event) {
@@ -153,12 +157,12 @@
                     name = $(me).attr('data-name');
                     method = events[name][event];
                     var result = true;
-                    if(false !== self.pre_trigger.call(me, e)) {
+                    if(false !== cls.pre_trigger.call(me, e)) {
                         if(typeof child[name][method] !== 'undefined'){
                             result = child[name][method].call(me, e);
                             console.log(name, method);
                         }
-                        self.post_trigger.call(me, e, result);
+                        cls.post_trigger.call(me, e, result);
                     }else{
                         console.log('event ' + event + ' for element:', $(me));
                         console.log('Method "' + method + '" was prevented from executing');
@@ -166,15 +170,15 @@
                 }
             };
         
-            for(fi in self.filter){
-                var filter = self.filter[fi];
+            for(fi in cls.filter){
+                var filter = cls.filter[fi];
             
                 filter.fields = filter.$el.find('[data-name]'),
                 filter.events = filter.$el.find('[data-on]');
         
                 $(filter.events).each(function (ke, el){
                     name = $(this).attr('data-name');
-                    events[name] = self.dispatchEvents.call(this);
+                    events[name] = cls.dispatchEvents.call(this);
                     for(event in events[name]) {
                         $(this).on(event, factory(this, event));
                     }
@@ -183,6 +187,6 @@
             
         };
         
-        self.bind(this);
+        cls.bind(this);
     };
 })( jQuery );
