c274e0e93b536fa78300f97e2b9492da1a097d0c
akindziora <alexander.kindziora@crowdguru.de>
2017-12-07 11:05:53 +0100

    json patch rfc added

--- GruntFile.js
+++ GruntFile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
                 banner: '/*! <%= pkg.name %> Version: <%= pkg.version %> <%= grunt.template.today("dd-mm-yyyy HH:MM:ss") %> */\n'
             },
             dist: {
-                src: ['components/skeleton.js', 'components/dom.js', 'components/data.js', 'klaster.js'],
+                src: ['components/skeleton.js', 'components/dom.js', 'bower_components/fast-json-patch/dist/fast-json-patch.js', 'components/data.js', 'klaster.js'],
                 dest: 'build/klaster.dev.js'
             }
         },
